93359caea3695f69b5902807eaec46258db15d62
filippossavva <filippos.97.boom@gmail.com>
2020-08-03 02:27:43 +0300

    character movement

--- SolvingHistory/Assets/character_movement.cs
+++ SolvingHistory/Assets/character_movement.cs
@@ -5,24 +5,18 @@ using UnityEngine;
 public class character_movement : MonoBehaviour
 {
     // Start is called before the first frame update
-    float horizontal = Input.GetAxis("Horizontal");
-    float vertical = Input.GetAxis("Vertical");
-    float speed = 5;
-    Rigidbody rigidBody;
+    public Animator anim;
+    //Rigidbody rigidBody;
 
     void Start()
     {
-        rigidBody = GetComponent<Rigidbody>();        
+        //rigidBody = GetComponent<Rigidbody>();        
     }
 
     // Update is called once per frame
     void Update()
     {
-        float movementSpeed = speed * Time.deltaTime;
-       
-        if(Input.GetKey(KeyCode.Space))
-        {
-            rigidBody.AddRelativeForce(Vector3.up * movementSpeed);
-        }
+        anim.SetFloat("vertical", Input.GetAxis("Vertical"));
+        anim.SetFloat("horizontal", Input.GetAxis("Horizontal"));
     }
 }
