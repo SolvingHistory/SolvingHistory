bc02779f42a4924d07cdf8be9f030d85b02575ad
makis97 <makis080997@gmail.com>
2020-08-05 00:40:39 +0300

    CHANGES

--- SolvingHistory/Assets/CameraScript.cs
+++ SolvingHistory/Assets/CameraScript.cs
@@ -6,7 +6,7 @@ public class CameraScript : MonoBehaviour
 {
 
     public float speedH = 2.0f;
-    public float speedV = 0.0f;
+    public float speedV = 2.0f;
 
     private float yaw = 0.0f;
     private float pitch = 0.0f;
