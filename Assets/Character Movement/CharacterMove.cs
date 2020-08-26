019359a217a548b8ce566ef5e3d6b3b8ec449a74
makis97 <makis080997@gmail.com>
2020-08-26 12:50:39 +0300

    tries with characters

--- Assets/Character Movement/CharacterMove.cs
+++ Assets/Character Movement/CharacterMove.cs
@@ -8,6 +8,9 @@ public class CharacterMove : MonoBehaviour
     
     CharacterController _characterController;
     private Vector3 _moveDirection;
+    static Animator anim;
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
 
     void Awake() => _characterController = GetComponent<CharacterController>();
     void FixedUpdate()
@@ -32,4 +35,61 @@ public class CharacterMove : MonoBehaviour
         _characterController.Move(_moveDirection);
     }
     private bool PlayerJumped => _characterController.isGrounded && Input.GetKey(KeyCode.Space);
+     void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        
+        transform.Translate(0,0,translation);
+        transform.Rotate(0,rotation,0);
+        
+        if(Input.GetButtonDown("Jump"))
+        {
+            anim.SetTrigger("isJumping");
+        }
+         //Walking
+        if (Input.GetKey(KeyCode.W))
+        {
+            anim.SetBool("isWalking", true);
+            anim.SetBool("isIdle", false);
+            anim.SetBool("isRunning", false);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            anim.SetBool("isWalking", true);
+            anim.SetBool("isIdle", false);
+            anim.SetBool("isRunning", false);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            anim.SetBool("isWalking", true);
+            anim.SetBool("isIdle", false);
+            anim.SetBool("isRunning", false);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            anim.SetBool("isWalking", true);
+            anim.SetBool("isIdle", false);
+            anim.SetBool("isRunning", false);
+        }
+        else if (Input.GetKey(KeyCode.C))
+        {
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isIdle", false);
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            //Is Idle
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isIdle", true);
+            anim.SetBool("isRunning", false);
+        }
+}
 }
